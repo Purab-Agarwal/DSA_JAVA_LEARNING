63412cb4c76e615fcd975d229ac70023021f8021
Purab Agarwal <purabagarwal10k@gmail.com>
2026-06-05 19:13:13 +0530

    Started Learning Loops today and praticed some basic problems on loop

---
+++ src/loops/Basic_Loop.java
@@ -0,0 +1,10 @@
+package loops;
+
+public class Basic_Loop {
+    public static void main(String[] args) {
+        int i;
+        for(i=1; i<=10; i++){
+            System.out.print(i*17+ " ");
+        }
+    }
+}
